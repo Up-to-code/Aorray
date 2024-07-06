5627c145563dd0adb042bd3de557aa9e29b295d9
Ahmed_Mansour <78514942+Up-to-code@users.noreply.github.com>
2024-07-06 03:41:54 +0300

    add home  page
    add home Screen  and  call  api

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,85 +1,105 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { FlatList, Image, RefreshControl, Text, View } from "react-native";
+import {
+  Alert,
+  FlatList,
+  Image,
+  RefreshControl,
+  Text,
+  View,
+} from "react-native";
 
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
+import useAppWrite from "../../lib/useAppwrit";
+import VideoCard from "../../components/VideoCard";
 
 const Home = () => {
   const [refreshing, setRefreshing] = useState(false);
+  const { Dtata, loding, refechData } = useAppWrite(getAllPosts);
+  const { Dtata : TrendingDtata } = useAppWrite(getLatestPosts);
 
-  const onRefresh = () => {
+
+  const onRefresh = async () => {
     setRefreshing(true);
-    setTimeout(() => {
-      setRefreshing(false);
-    }, 2000);
+    await refechData();
+    setRefreshing(false);
   };
-  const posts = [
-    { $id: "1", title: "Post 1" },
-    { $id: "2", title: "Post 2" },
-    { $id: "3", title: "Post 3" },
-    { $id: "4", title: "Post 4" },
-    { $id: "5", title: "Post 5" },
-    { $id: "6", title: "Post 6" },
-  ];
+
   // one flatlist
   // with list header
   // and horizontal flatlist
 
   //  we cannot do that with just scrollview as there's both horizontal and vertical scroll (two flat lists, within trending)
-
+ 
   return (
     <SafeAreaView className="bg-primary h-full">
-      <FlatList
-        data={posts}
-        keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => (
-          <Text className="text-white"> {item.title}</Text>
-        )}
-        ListHeaderComponent={() => (
-          <View className="flex my-6 px-4 space-y-6">
-            <View className="flex justify-between items-start flex-row mb-6">
-              <View>
-                <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
-                </Text>
-                <Text className="text-2xl font-psemibold text-white">
-                  JSMastery
-                </Text>
+      {!Date ? (
+        <View className="flex-1 items-center justify-center">
+          <Text className="text-white">Loading</Text>
+        </View>
+      ) : (
+        <FlatList
+          data={Dtata}
+          keyExtractor={(item) => item?.$id}
+          renderItem={({ item }) => (
+            <VideoCard
+              title={item?.title}
+              creator={item?.UseName}
+              avatar={item.Avtar}
+              thumbnail={item?.thamnil}
+              video={item?.video}
+            />
+          )}
+          ListHeaderComponent={() => (
+            <View className="flex my-6 px-4 space-y-6">
+              <View className="flex justify-between items-start flex-row mb-6">
+                <View>
+                  <Text className="font-pmedium text-sm text-gray-100">
+                    Welcome Back
+                  </Text>
+                  <Text className="text-2xl font-psemibold text-white">
+                    JSMastery
+                  </Text>
+                </View>
+
+                <View className="mt-1.5">
+                  <Image
+                    source={images.logoSmall}
+                    className="w-9 h-10"
+                    resizeMode="contain"
+                  />
+                </View>
               </View>
 
-              <View className="mt-1.5">
-                <Image
-                  source={images.logoSmall}
-                  className="w-9 h-10"
-                  resizeMode="contain"
-                />
+              <SearchInput otherStyles={"m-0"} />
+
+              <View className="w-full flex-1 pt-5 pb-8">
+                <Text className="text-lg font-pregular text-gray-100 mb-3">
+                  Latest Videos
+                </Text>
+
+                <Trending posts={TrendingDtata ?? []} />
               </View>
             </View>
-
-            <SearchInput otherStyles={"m-0"} />
-
-            <View className="w-full flex-1 pt-5 pb-8">
-              <Text className="text-lg font-pregular text-gray-100 mb-3">
-                Latest Videos
-              </Text>
-
-              <Trending posts={posts ?? []} />
-            </View>
-          </View>
-        )}
-        ListEmptyComponent={() => (
-          <EmptyState
-            title="No Videos Found"
-            subtitle="No videos created yet"
-          />
-        )}
-        refreshControl={
-          <RefreshControl refreshing={refresh} onRefresh={() => onRefresh()} />
-        }
-      />
+          )}
+          ListEmptyComponent={() => (
+            <EmptyState
+              title="No Videos Found"
+              subtitle="No videos created yet"
+            />
+          )}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={() => onRefresh()}
+            />
+          }
+        />
+      )}
     </SafeAreaView>
   );
 };
